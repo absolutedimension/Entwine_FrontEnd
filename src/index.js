047bb174bbab6e092d86f53be387751b2af257cb
absolutedimension <dk.peace@gmail.com>
2020-05-10 18:43:27 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,66 @@
+/*
+
+=========================================================
+* Now UI Kit React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/now-ui-kit-react
+* Copyright 2019 Creative Tim (http://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/now-ui-kit-react/blob/master/LICENSE.md)
+
+* Designed by www.invisionapp.com Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+// styles for this kit
+import "assets/css/bootstrap.min.css";
+import "assets/scss/now-ui-kit.scss";
+import "assets/demo/demo.css";
+import "assets/demo/nucleo-icons-page-styles.css";
+// pages for this kit
+import Index from "views/Index.js";
+import NucleoIcons from "views/NucleoIcons.js";
+import LoginPage from "views/examples/LoginPage.js";
+import LandingPage from "views/examples/LandingPage.js";
+import ProfilePage from "views/examples/ProfilePage.js";
+import SignUp from "views/examples/SignUp.js";
+import EntwineMorning from "views/examples/EntwineMorning.js";
+import EntwineYourself from "views/examples/EntwineYourself.js";
+import EntwineTrance from "views/examples/EntwineTrance.js";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Switch>
+        <Route path="/index" render={props => <Index {...props} />} />
+        <Route
+          path="/nucleo-icons"
+          render={props => <NucleoIcons {...props} />}
+        />
+        <Route
+          path="/landing-page"
+          render={props => <LandingPage {...props} />}
+        />
+        <Route
+          path="/profile-page"
+          render={props => <ProfilePage {...props} />}
+        />
+        <Route path="/login-page" render={props => <LoginPage {...props} />} />
+        <Route path="/signup-page" render={props => <SignUp {...props} />} />
+        <Route path="/entwine-morning" render={props => <EntwineMorning {...props} />} />
+        <Route path="/entwine-yourself" render={props => <EntwineYourself {...props} />} />
+        <Route path="/entwine-trance" render={props => <EntwineTrance {...props} />} />
+        <Redirect to="/index" />
+        <Redirect from="/" to="/index" />
+      </Switch>
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
---
+++ src/views/examples/EntwineTrance.js
@@ -0,0 +1,227 @@
+import React from "react";
+
+// reactstrap components
+import {
+  Button,
+  NavItem,
+  NavLink,
+  Nav,
+  TabContent,
+  TabPane,
+  Container,
+  Row,
+  Col,
+  UncontrolledTooltip
+} from "reactstrap";
+
+// core components
+import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
+import EntwineTranceHeader from "components/Headers/EntwineTranceHeader.js";
+import DefaultFooter from "components/Footers/DefaultFooter.js";
+
+function EntwineTrance() {
+  const [pills, setPills] = React.useState("2");
+  React.useEffect(() => {
+    document.body.classList.add("profile-page");
+    document.body.classList.add("sidebar-collapse");
+    document.documentElement.classList.remove("nav-open");
+    return function cleanup() {
+      document.body.classList.remove("profile-page");
+      document.body.classList.remove("sidebar-collapse");
+    };
+  });
+  return (
+    <>
+      <ExamplesNavbar />
+      <div className="wrapper">
+        <EntwineTranceHeader />
+        <div className="section">
+          <Container>
+            {/* <div className="button-container">
+              <Button className="btn-round" color="info" size="lg">
+                Follow
+              </Button>
+              <Button
+                className="btn-round btn-icon"
+                color="default"
+                id="tooltip515203352"
+                size="lg"
+              >
+                <i className="fab fa-twitter"></i>
+              </Button>
+              <UncontrolledTooltip delay={0} target="tooltip515203352">
+                Follow me on Twitter
+              </UncontrolledTooltip>
+              <Button
+                className="btn-round btn-icon"
+                color="default"
+                id="tooltip340339231"
+                size="lg"
+              >
+                <i className="fab fa-instagram"></i>
+              </Button>
+              <UncontrolledTooltip delay={0} target="tooltip340339231">
+                Follow me on Instagram
+              </UncontrolledTooltip>
+            </div> */}
+            <h4 className="title">Music transports</h4>
+            <h4 className="title">Music shields</h4>
+            <h4 className="title">Music heals </h4>
+            <h4 className="title">Music leads us cloder to God </h4>
+            <h3 className="description">
+            What does this mean? Why do most cultures start their days with spiritual music or prayers?
+            </h3>
+            <h5>Music has the power to bring the mind, body, and spirit in unison. It is one of the most powerful mediums to inspire people’s imagination, enhance awareness, and help them reach out for their goals with energy and attention.</h5>
+            <h5>By offering back the gift of music to the Creator through singing and praising, we open ourselves up to a bundle of opportunities, extreme will and infinite possibilities. It releases the brain's natural "happy chemicals" and encompasses
+               you with a cloud of freshness and gratitude as you continue towards the rest of your day. Tune in to a morning session to experience the magic.</h5>
+            
+            <Row>
+              <Col className="ml-auto mr-auto" md="6">
+                <h4 className="title text-center">My Portfolio</h4>
+                <div className="nav-align-center">
+                  <Nav
+                    className="nav-pills-info nav-pills-just-icons"
+                    pills
+                    role="tablist"
+                  >
+                    <NavItem>
+                      <NavLink
+                        className={pills === "1" ? "active" : ""}
+                        href="#pablo"
+                        onClick={e => {
+                          e.preventDefault();
+                          setPills("1");
+                        }}
+                      >
+                        <i className="now-ui-icons design_image"></i>
+                      </NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink
+                        className={pills === "2" ? "active" : ""}
+                        href="#pablo"
+                        onClick={e => {
+                          e.preventDefault();
+                          setPills("2");
+                        }}
+                      >
+                        <i className="now-ui-icons location_world"></i>
+                      </NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink
+                        className={pills === "3" ? "active" : ""}
+                        href="#pablo"
+                        onClick={e => {
+                          e.preventDefault();
+                          setPills("3");
+                        }}
+                      >
+                        <i className="now-ui-icons sport_user-run"></i>
+                      </NavLink>
+                    </NavItem>
+                  </Nav>
+                </div>
+              </Col>
+              <TabContent className="gallery" activeTab={"pills" + pills}>
+                <TabPane tabId="pills1">
+                  <Col className="ml-auto mr-auto" md="10">
+                    <Row className="collections">
+                      <Col md="6">
+                        <img
+                          alt="..."
+                          className="img-raised"
+                          src={require("assets/img/bg1.jpg")}
+                        ></img>
+                        <img
+                          alt="..."
+                          className="img-raised"
+                          src={require("assets/img/bg3.jpg")}
+                        ></img>
+                      </Col>
+                      <Col md="6">
+                        <img
+                          alt="..."
+                          className="img-raised"
+                          src={require("assets/img/bg8.jpg")}
+                        ></img>
+                        <img
+                          alt="..."
+                          className="img-raised"
+                          src={require("assets/img/bg7.jpg")}
+                        ></img>
+                      </Col>
+                    </Row>
+                  </Col>
+                </TabPane>
+                <TabPane tabId="pills2">
+                  <Col className="ml-auto mr-auto" md="10">
+                    <Row className="collections">
+                      <Col md="6">
+                        <img
+                          alt="..."
+                          className="img-raised"
+                          src={require("assets/img/bg6.jpg")}
+                        ></img>
+                        <img
+                          alt="..."
+                          className="img-raised"
+                          src={require("assets/img/bg11.jpg")}
+                        ></img>
+                      </Col>
+                      <Col md="6">
+                        <img
+                          alt="..."
+                          className="img-raised"
+                          src={require("assets/img/bg7.jpg")}
+                        ></img>
+                        <img
+                          alt="..."
+                          className="img-raised"
+                          src={require("assets/img/bg8.jpg")}
+                        ></img>
+                      </Col>
+                    </Row>
+                  </Col>
+                </TabPane>
+                <TabPane tabId="pills3">
+                  <Col className="ml-auto mr-auto" md="10">
+                    <Row className="collections">
+                      <Col md="6">
+                        <img
+                          alt="..."
+                          className="img-raised"
+                          src={require("assets/img/bg3.jpg")}
+                        ></img>
+                        <img
+                          alt="..."
+                          className="img-raised"
+                          src={require("assets/img/bg8.jpg")}
+                        ></img>
+                      </Col>
+                      <Col md="6">
+                        <img
+                          alt="..."
+                          className="img-raised"
+                          src={require("assets/img/bg7.jpg")}
+                        ></img>
+                        <img
+                          alt="..."
+                          className="img-raised"
+                          src={require("assets/img/bg6.jpg")}
+                        ></img>
+                      </Col>
+                    </Row>
+                  </Col>
+                </TabPane>
+              </TabContent>
+            </Row>
+          </Container>
+        </div>
+        <DefaultFooter />
+      </div>
+    </>
+  );
+}
+
+export default EntwineTrance;
